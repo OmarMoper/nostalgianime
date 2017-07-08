d8a519f66879600e44ce34e77b7ddfa31adb32e1
Omar Mohamad - El Hassan Lopesino <omar.lopesino@metadrop.net>
2017-07-08 19:18:09 +0200

    Document class RandomList

--- src/app/services/List/RandomList.ts
+++ src/app/services/List/RandomList.ts
@@ -1,18 +1,56 @@
+/**
+ * List which returns random elements, never repeating.
+ */
 export class RandomList {
-    
+
+    /**
+     * Elements list.
+     *
+     * @var array
+     */
     protected list: Array<any>;
+
+    /**
+     * Elements previously used.
+     *
+     * @var array
+     */
     protected used: Array<any>;
+
+    /**
+     * Number of times a element won't be repeated.
+     *
+     * @var int
+     */
     protected turns = 2;
 
+    /**
+     * Constructor.
+     *
+     * @param array list
+     *   List of elements.
+     */
     constructor(list) {
         this.list = list;
         this.used = [];
     }
 
+    /**
+     * Get random element from list.
+     *
+     * @return any
+     *   Random element.
+     */
     public randomElement() {
         return this.list[this.randomElementKey()];
     }
 
+    /**
+     * Get random element key from list.
+     *
+     * @return any
+     *   Random element.
+     */
     public randomElementKey() {
         this.free();
         let element = this.getRandomElement();
@@ -20,6 +58,12 @@ export class RandomList {
         return element;
     }
 
+    /**
+     * Process random element from list.
+     *
+     * @return any
+     *   Random element.
+     */
     public getRandomElement() {
         let valid = false;
         let element = null;
@@ -30,20 +74,38 @@ export class RandomList {
         return element;
     }
 
+    /**
+     * Check element is used previously.
+     *
+     * @return bool
+     *   Element is used previously?
+     */
     public isUsed(element) {
         return this.used.indexOf(element) >= 0;
     }
 
+    /**
+     * Record element has been used.
+     *
+     * @param int element
+     *   Element key.
+     */
     public record(element) {
         this.used.push(element);
     }
 
+    /**
+     * Free elements used when there are more than needed.
+     */
     public free() {
         if (this.used.length > this.turns) {
             this.used.shift();
         }
     }
 
+    /**
+     * Get list length.
+     */
     public getLength() {
         return this.list.length;
     }
