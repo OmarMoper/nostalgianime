051965f2d115d2a4bd3365bd7e07cc1528598d57
Omar Mohamad - El Hassan Lopesino <omar.lopesino@metadrop.net>
2017-07-08 15:01:04 +0200

    Back url: add https

--- src/app/services/API/AbstractAPI.ts
+++ src/app/services/API/AbstractAPI.ts
@@ -1,6 +1,6 @@
 import { Http } from '@angular/http';
 
-const API_URL = 'http://nostalgiame.herokuapp.com/api';
+const API_URL = 'https://nostalgiame.herokuapp.com/api';
 export abstract class AbstractAPI {
 
     constructor(protected http: Http) {}
